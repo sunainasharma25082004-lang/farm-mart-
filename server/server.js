854befed535c407acda2bd9e9063ee65dac0fb55
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2026-08-07 10:29:02 +0530

    done

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 // In-memory data store for partner applications & inquiries
 const applications = [];
