07a5db6fbd840622b40bdbecd8a0f1c550684705
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2026-09-12 17:21:29 +0530

    code added

--- userApp/src/context/AppContext.js
+++ userApp/src/context/AppContext.js
@@ -69,7 +69,7 @@ export const AppProvider = ({ children }) => {
 
   const fetchProducts = async () => {
     const data = await apiService.getProducts();
-    if (data && data.success && Array.isArray(data.products)) {
+    if (data && data.success && Array.isArray(data.products) && data.products.length > 0) {
       setProducts(data.products.map(normalizeProduct));
     }
   };
